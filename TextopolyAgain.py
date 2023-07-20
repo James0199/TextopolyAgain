b365466eb3e09a604a122363e143e1cd142bda05
James0199 <jamesjohnson.20901@gmail.com>
2023-07-19 22:35:07 -0400

    putting

--- TextopolyAgain.py
+++ TextopolyAgain.py
@@ -1,8 +1,6 @@
 from random import randint
 from time import sleep
 
-# Welcome to my potentially better code than the old one
-
 class Player:
     def __init__(self, location, balance, properties, extras):
         self.location = location
@@ -10,18 +8,24 @@ class Player:
         self.properties = properties
         self.extras = extras
 
-# Setting player count
-while True:
-    playerCount = input("How many players?(2-8):")
-    if not playerCount.isdigit():
-        print("Try again")
-        continue
-    if not int(playerCount) in range(2, 8+1):
-        print("Try again")
-        continue
-    break
+def playerSetup():
+    # Setting player count
+    while True:
+        playerCount = input("How many players?(2-8):")
+        if not playerCount.isdigit():
+            print("Try again")
+            continue
+        if not int(playerCount) in range(2, 8+1):
+            print("Try again")
+            continue
+        break
+    
+    # Creating player data
+    playerList = {}
+    for count in range(1, playerCount+1):
+        playerList.update({count: Player(0, 1500, [], {"Jail": False, "JailOutFree": False, "Doubles": 0})})
+    
+    return playerList
 
-# Creating player data
-playerList = {}
-for count in range(1, playerCount+1):
-    playerList.update({count: Player(0, 1500, [], {"Jail": False, "JailOutFree": False, "Doubles": 0})})
+# Setting up players and player data
+players = playerSetup()
