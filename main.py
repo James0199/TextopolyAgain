b0ef2a09b95abbbb8d2a08402f0622c65c90d4c7
Heet Patel <75808215+patelheet30@users.noreply.github.com>
2023-07-22 15:41:10 +0100

    Refactor square loading in main.py
    Removed the load_squares() method within main.py and replaced it with an imported function from property_collecters for cleaner code. This change was done to adhere to DRY principles. Importing the function increases readability and maintainability as square loading logic now has a single source of truth.

--- main.py
+++ main.py
@@ -1,37 +1,12 @@
 from Models.game import Game
 from Models.player import Player
-from Models.square import Square
+from property_collecters import load_squares
 
-
-def load_squares() -> dict[int, Square]:
-    squares_list: dict[int, Square] = {}
-    with open("squares.txt", "r") as squaresFile:
-        squares = eval(squaresFile.read())
-
-    count = 0
-
-    for square in squares:
-        squares_list[count] = Square(
-                name=squares[square]["name"],
-                square_type=squares[square]["type"],
-                price=squares[square].setdefault("price", None),
-                color=squares[square].setdefault("color", None),
-                owner=squares[square].setdefault("owner", None),
-                improvement_lvl=squares[square].setdefault("improvement_lvl", None),
-                improvement_price=squares[square].setdefault("improvement_price", None),
-                mortgaged=squares[square].setdefault("mortgaged", False),
-                rent_levels=squares[square].setdefault("rent_levels", None)
-            )
-        count += 1
-
-    return squares_list
 
 def start():
     print("Game is initialising...")
 
-    squares_for_game = load_squares()
-
-    player_list: list[Player] =  []
+    player_list: list[Player] = []
 
     while True:
         try:
@@ -43,12 +18,7 @@ def start():
             print("Enter a valid number")
 
 
+all_squares = load_squares()
 
-    for count in range(0, player_count):
-        player_list.append(
-            Player(
-                location=squares_for_game[0],
-            )
-        )
-
-    new_game = Game()
+for square in all_squares:
+    print(square.name)
