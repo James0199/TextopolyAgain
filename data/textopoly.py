a8df86215b31a7dfaffb271691db69a67b961a02
James0199 <jamesjohnson.20901@gmail.com>
2023-07-31 04:53:23 -0400

    updated old code

--- data/textopoly.py
+++ data/textopoly.py
@@ -1,14 +1,18 @@
-from random import randint, choice
 from os import path
+from random import randint
+
 
 class Player:
-    def __init__(self, location: int,
-                 balance: int,
-                 properties: list,
-                 jail: bool,
-                 jail_out_free: bool,
-                 in_jail_turns: int,
-                 doubles: int,):
+    def __init__(
+        self,
+        location: int,
+        balance: int,
+        properties: list,
+        jail: bool,
+        jail_out_free: bool,
+        in_jail_turns: int,
+        doubles: int,
+    ):
         self.location = location
         self.balance = balance
         self.properties = properties
@@ -16,23 +20,19 @@ class Player:
         self.jail_out_free = jail_out_free
         self.in_jail_turns = in_jail_turns
         self.doubles = doubles
-    
 
-    def normal_turn(self, jail_doubles,
-                    roll_one=0, roll_two=0):
+    def normal_turn(self, jail_doubles, roll_one=0, roll_two=0):
         if not jail_doubles:
             roll_one, roll_two = dice_roll()
-            print(f"1st: {roll_one}, 2nd: {roll_two} = "
-                  f"{roll_one + roll_two}")
+            print(f"1st: {roll_one}, 2nd: {roll_two} = " f"{roll_one + roll_two}")
 
             self.doubles_count(roll_one, roll_two)
-        
+
         input()
         self.advance(roll_one + roll_two)
         current_square = squares[self.location]
-        print(f"New square:\n{self.location} - "
-              f"{current_square['name']}")
-        
+        print(f"New square:\n{self.location} - " f"{current_square['name']}")
+
         self.landing_square(current_square)
 
     def landing_square(self, current_square):
@@ -85,7 +85,6 @@ class Player:
         elif card["type"] == "go":
             self.location = 0
             self.balance += 200
-        
 
     def go_to_jail(self):
         self.jail = True
@@ -110,29 +109,27 @@ class Player:
         option = input("Enter choice:")
 
         self.jail_options(option)
-    
+
     def jail_options(self, option):
         if option == "r" and self.in_jail_turns <= 3:
             roll_one, roll_two = dice_roll()
             print(f"1st roll: {roll_one}, 2nd roll: {roll_two}")
 
             if roll_one == roll_two:
-                print("You rolled a double!"
-                      "\nYou've been released")
-                self.get_out_of_jail(squares, (False, roll_one, roll_two))
+                print("You rolled a double!" "\nYou've been released")
+                self.get_out_of_jail((False, roll_one, roll_two))
                 return
-            print("You didn't roll a double"
-                  "\nYou'll remain in Jail")
+            print("You didn't roll a double" "\nYou'll remain in Jail")
             return
-        
+
         elif option == "f" and self.jail_out_free:
             print("You have used your Get Out Of Jail Free Card")
-            self.get_out_of_jail(squares, False)
+            self.get_out_of_jail(False)
             return
-        
+
         print("You've paid $50 bail to get out of jail")
         self.balance -= 50
-        self.get_out_of_jail(squares, False)
+        self.get_out_of_jail(False,)
         return
 
     def get_out_of_jail(self, double_roll=(False, 0, 0)):
@@ -140,12 +137,13 @@ class Player:
         self.jail = False
         self.normal_turn(jail_doubles, roll_one, roll_two)
 
-
     def print_stats(self, player_num, current_square):
-        print(f"\nPlayer {player_num}'s turn"
-              f"\nCurrent balance: {self.balance}"
-              f"\nCurrent square:\n{self.location} - {current_square['name']}")
-    
+        print(
+            f"\nPlayer {player_num}'s turn"
+            f"\nCurrent balance: {self.balance}"
+            f"\nCurrent square:\n{self.location} - {current_square['name']}"
+        )
+
     def advance(self, moves):
         if self.location + moves > 39:
             self.location = (self.location + moves) - 40
@@ -156,8 +154,8 @@ class Player:
 
     def doubles_count(self, roll_one, roll_two):
         if roll_one == roll_two:
-                print("Doubles!")
-                self.doubles += 1
+            print("Doubles!")
+            self.doubles += 1
         else:
             self.doubles = 0
 
@@ -168,44 +166,49 @@ def player_setup():
         if not player_count.isdigit():
             print("Try again")
             continue
-        if not int(player_count) in range(2, 8+1):
+        if not int(player_count) in range(2, 8 + 1):
             print("Try again")
             continue
         player_count = int(player_count)
         break
-    
+
     player_list = {}
-    for count in range(1, player_count+1):
+    for count in range(1, player_count + 1):
         # Player(location, balance, properties, jail, jail_out_free, in_jail_turns, doubles)
         player_list.update({count: Player(0, 1500, [], False, False, 0, 0)})
-    
-    return player_list, [i for i in range(1, player_count+1)]
+
+    return player_list, [i for i in range(1, player_count + 1)]
+
 
 def file_setup():
-    file_list = ["data/squares.py", "data/com_chest.py", "data/chance.py",
-                 "TextopolyAgain.py"]
+    file_list = [
+        "data/squares.py",
+        "data/com_chest.py",
+        "data/chance.py",
+        "TextopolyAgain.py",
+    ]
 
     for file in file_list:
         if not path.isfile(file):
-            print(f'File "{file}" is missing'
-                  '\nPlease download all files')
+            print(f'File "{file}" is missing' "\nPlease download all files")
 
     global squares, com_chest, chance
 
-    with open("data/squares.py", "r+") as squares_file, \
-         open("data/com_chest", "r+") as com_chest_file, \
-         open("data/chance.py", "r+") as chance_file:
-        
+    with open("data/squares.py", "r+") as squares_file, open(
+        "data/com_chest", "r+"
+    ) as com_chest_file, open("data/chance.py", "r+") as chance_file:
+
         squares = eval(squares_file.read())
         com_chest_list, com_chest = eval(com_chest_file.read())
         chance_list, chance = eval(chance_file.read())
-    
+
 
 def turnAdvance(player_num, remaining_players):
     if player_num == max(remaining_players):
         return min(remaining_players)
-    
+
     return remaining_players[remaining_players.index(player_num) + 1]
+
 
 def dice_roll():
     input("\nRoll dice >")
